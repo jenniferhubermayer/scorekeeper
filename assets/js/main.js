8b427b75f8e2de3aa3fb66f58a01753a282045bb
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-04-30 13:57:27 +0200

    Counter works, CSS angepasst.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,61 @@
-console.log('It works!')
+console.log('It works!');
 
 // Variablen
 
 let scoreHome = 0;
 let scoreAway = 0;
-let countHome = document.querySelector('.points-home')
-let countAway = document.querySelector('.points-away')
+let countHome = document.querySelector('.points-home');
+let countAway = document.querySelector('.points-away');
 
 console.log(scoreHome);
 console.log(scoreAway);
 
+countHome.innerHTML = '0' + scoreHome;
+countAway.innerHTML = '0' + scoreAway;
+
 // Funktionen Home
 
 function pointsOnTopOneHome(){
-    scoreHome = scoreHome + 1;
-    console.log('Punktstand Home: ' + scoreHome)
+    scoreHome += 1;
+    console.log('Punktstand Home: ' + scoreHome);
     if(scoreHome <= 10){    
         countHome.innerHTML = '0' + scoreHome;
-    }
+    };
     if(scoreHome >= 10)
    {
     countHome.innerHTML = scoreHome;
-    }
+    };
 }
 
 function pointsOnTopTwoHome(){
-    scoreHome = scoreHome + 2;
+    scoreHome += 2;
+    console.log('Punktstand Home: ' + scoreHome);
     if(scoreHome <= 10){    
         countHome.innerHTML = '0' + scoreHome;
-    }
+    };
     if(scoreHome >= 10)
    {
     countHome.innerHTML = scoreHome;
-    }
+    };
 }
 
 function pointsOnTopThreeHome(){
-    scoreHome = scoreHome + 3;
+    scoreHome += 3;
+    console.log('Punktstand Home: ' + scoreHome);
     if(scoreHome <= 10){    
         countHome.innerHTML = '0' + scoreHome;
-    }
+    };
     if(scoreHome >= 10)
    {
     countHome.innerHTML = scoreHome;
-    }
+    };
 }
-
-pointsOnTopOneHome();
 
 // Funktionen Away
 
 function pointsOnTopOneAway(){
-    scoreAway = scoreAway + 1;
+    scoreAway += 1;
+    console.log('Punktstand Away: ' + scoreAway)
     if(scoreAway <= 10){    
         countAway.innerHTML = '0' + scoreAway;
     }
@@ -62,7 +66,7 @@ function pointsOnTopOneAway(){
 }
 
 function pointsOnTopTwoAway(){
-    scoreAway = scoreAway + 2;
+    scoreAway += 2;
     if(scoreAway <= 10){    
         countAway.innerHTML = '0' + scoreAway;
     }
@@ -73,7 +77,7 @@ function pointsOnTopTwoAway(){
 }
 
 function pointsOnTopThreeAway(){
-    scoreAway = scoreAway + 3;
+    scoreAway += 3;
     if(scoreAway <= 10){    
         countAway.innerHTML = '0' + scoreAway;
     }
@@ -83,9 +87,11 @@ function pointsOnTopThreeAway(){
     }
 }
 
-pointsOnTopOneAway();
+// Funktion für den Reset
 
-// Ausgabe 
-
-countHome.innerHTML = '00'
-countAway.innerHTML = '00'
+function resetPoints(){
+    scoreAway = 0;
+    scoreHome = 0;
+    countHome.innerHTML = '0' + 0;
+    countAway.innerHTML = '0' + 0;
+}
