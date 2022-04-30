a98b09dd9790befbbd6a7a061de8153b55d00c01
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-04-30 16:39:57 +0200

    Erste Versuche die Bonmusaufgabe zu lösen

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,43 @@ function resetPoints(){
     countHome.innerHTML = '0' + 0;
     countAway.innerHTML = '0' + 0;
 }
+
+// BONUS
+
+// Hier wird das Modal aufgerufen und geschlossen.
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("rename");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  } 
+
+// Hier versuche ich den Text in das HTML zu überführen.
+
+let nameHome ='HansPeter'
+
+let renameHome = document.getElementById('change-home-team-name');
+
+console.log(nameHome);
+
+nameHome.innerHTML = nameHome
+
+function renameTeams(){
+    nameHome = nameHome;
+    console.log('Hier ist der Name aus der Box' + nameHome);
+    renameHome.innerHTML = nameHome;
+}
+
+renameTeams();
